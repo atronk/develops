97011c09d3f8214bb873ae61810009b72331283f
artpheon <idcdtokms@gmail.com>
2021-12-20 23:23:10 +0300

    removed trash & added heroku

--- news_board/settings.py
+++ news_board/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,6 +80,8 @@ WSGI_APPLICATION = "news_board.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES['default'] = dj_database_url.parse(os.environ['DATABASE_URL'])
 
 DATABASES = {
     'default': {
